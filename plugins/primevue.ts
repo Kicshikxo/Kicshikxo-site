37dcabfb6b936b2e887c07eb7f49e825e35d6c1e
Kicshikxo <supermegavalera9000@gmail.com>
2022-11-28 23:54:46 +0300

    Format primevue plugin

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,10 +1,10 @@
-import { defineNuxtPlugin } from "#app";
-import PrimeVue from "primevue/config";
-import Button from "primevue/button";
-import Chip from 'primevue/chip';
+import { defineNuxtPlugin } from "#app"
+import PrimeVue from "primevue/config"
+import Button from "primevue/button"
+import Chip from 'primevue/chip'
 
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, { ripple: false });
-    nuxtApp.vueApp.component('PButton', Button);
-    nuxtApp.vueApp.component('PChip', Chip);
-});
+export default defineNuxtPlugin(nuxtApp => {
+    nuxtApp.vueApp.use(PrimeVue, { ripple: false })
+    nuxtApp.vueApp.component('PButton', Button)
+    nuxtApp.vueApp.component('PChip', Chip)
+})
