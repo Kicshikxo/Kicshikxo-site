a9a8d6139bc6637e0056bb2695db0e02f204bc2f
Kicshikxo <supermegavalera9000@gmail.com>
2022-11-28 10:51:55 +0300

    Remove some features

--- components/feature.vue
+++ components/feature.vue
@@ -28,31 +28,7 @@
                 </span>
                 <div class="text-900 mb-3 font-medium">Easy to Use</div>
                 <span class="text-700 text-sm line-height-3">Ornare suspendisse sed nisi lacus sed viverra tellus. Neque
-                    volutpat ac tincidunt vitae semper.</span>
-            </div>
-            <div class="col-12 md:col-4 mb-4 px-5">
-                <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                    <i class="pi pi-globe text-4xl text-blue-500"></i>
-                </span>
-                <div class="text-900 mb-3 font-medium">Fast & Global Support</div>
-                <span class="text-700 text-sm line-height-3">Fermentum et sollicitudin ac orci phasellus egestas tellus
-                    rutrum tellus.</span>
-            </div>
-            <div class="col-12 md:col-4 mb-4 px-5">
-                <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                    <i class="pi pi-github text-4xl text-blue-500"></i>
-                </span>
-                <div class="text-900 mb-3 font-medium">Open Source</div>
-                <span class="text-700 text-sm line-height-3">Nec tincidunt praesent semper feugiat. Sed adipiscing diam
-                    donec adipiscing tristique risus nec feugiat. </span>
-            </div>
-            <div class="col-12 md:col-4 md:mb-4 mb-0 px-3">
-                <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                    <i class="pi pi-shield text-4xl text-blue-500"></i>
-                </span>
-                <div class="text-900 mb-3 font-medium">Trusted Securitty</div>
-                <span class="text-700 text-sm line-height-3">Mattis rhoncus urna neque viverra justo nec ultrices. Id
-                    cursus metus aliquam eleifend.</span>
+                    volutpat ac tincidunt vitae semper.sfs fdsf sdf sdf df sdf df sdfsd fdf sdfsdf sdf d</span>
             </div>
         </div>
     </div>
