596e385140e7924cadd4999d7c702cab416aaf6a
Kicshikxo <supermegavalera9000@gmail.com>
2022-11-28 11:27:57 +0300

    Improve hero section

--- components/hero.vue
+++ components/hero.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class='grid grid-nogutter surface-section text-800'>
-        <div class='col-12 md:col-6 p-6 text-center md:text-left flex align-items-center '>
+    <div class='grid grid-nogutter surface-section text-800 min-h-screen align-items-center'>
+        <div class='col-12 md:col-6 p-6 text-center md:text-left'>
             <section>
                 <div class="text-6xl font-bold flex flex-column">
                     <div class='text-6xl font-bold mb-1'>Привет, я <span class="text-primary">Kicshikxo</span></div>
                     <div class='text-5xl font-medium mb-1'>и я начинающий разработчик</div>
                 </div>
 
-                <p class='mt-0 mb-4 text-700 line-height-3 text-2xl'>Этот веб-сайт расскажет обо мне</p>
+                <p class='mt-1 mb-4 text-700 text-2xl'>Этот веб-сайт расскажет обо мне</p>
 
                 <nuxt-link to="https://github.com/Kicshikxo" target="blank" class="no-underline">
                     <p-button label="Github" type='button' class='mr-3 p-button-raised' icon="pi pi-github" />
@@ -19,7 +19,7 @@
             </section>
         </div>
         <div class='col-12 md:col-6 overflow-hidden'>
-            <img height="630" src='/images/hero.webp' alt='Image' class='md:ml-auto block'
+            <img width="100%" src='/images/hero.webp' alt='Image'
                 style='clip-path: polygon(8% 0, 100% 0%, 100% 100%, 0 100%)' />
         </div>
     </div>
