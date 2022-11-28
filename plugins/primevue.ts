1636f7dbf2d0ce921485fbfc43f30b99c51e4378
Kicshikxo <supermegavalera9000@gmail.com>
2022-11-28 07:58:38 +0300

    Disable primevue ripple

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -3,6 +3,6 @@ import PrimeVue from "primevue/config";
 import Button from "primevue/button";
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, { ripple: true });
+    nuxtApp.vueApp.use(PrimeVue, { ripple: false });
     nuxtApp.vueApp.component('PButton', Button);
 });
