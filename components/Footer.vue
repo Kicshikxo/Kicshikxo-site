1ca28e2d2acf7bfc6e32a3e7a6d9ecf368146ba9
Kicshikxo <supermegavalera9000@gmail.com>
2022-11-28 12:45:47 +0300

    Improve footer flex

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,8 @@
 <template>
     <footer class="surface-section px-4 md:px-6 lg:px-8">
-        <div class="py-6 flex flex-column sm:flex-row sm:align-items-center justify-content-between">
+        <div class="py-6 flex flex-column sm:flex-row align-items-center justify-content-between gap-2">
+            <div class="line-height-3">© {{ new Date().getFullYear() }} Kicshikxo, All rights reserved</div>
             <div>
-                <div class="mt-2 line-height-3">© {{ new Date().getFullYear() }} Kicshikxo, All rights reserved</div>
-            </div>
-            <div class="mt-3 sm:mt-0">
                 <nuxt-link to="https://github.com/Kicshikxo" target="blank"
                     class="cursor-pointer text-500 transition-colors transition-duration-150 hover:text-700">
                     <i class="pi pi-github text-xl" />
