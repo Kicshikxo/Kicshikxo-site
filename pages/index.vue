92eb4f3f59fb2169c55c069cd68ada362c3f3476
Kicshikxo <supermegavalera9000@gmail.com>
2022-11-29 00:07:31 +0300

    Add custom styles to scrolltop

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,24 @@
         <Repos />
         <!-- <Feature /> -->
         <Footer />
-        <PScrollTop />
+        <PScrollTop class="custom-scrolltop" />
     </div>
 </template>
+
+<style scoped>
+::v-deep(.custom-scrolltop) {
+    width: 2rem;
+    height: 2rem;
+    border-radius: 4px;
+    background-color: var(--primary-color);
+}
+
+::v-deep(.custom-scrolltop):hover {
+    background-color: var(--primary-color);
+}
+
+::v-deep(.custom-scrolltop) .p-scrolltop-icon {
+    font-size: 1rem;
+    color: var(--primary-color-text);
+}
+</style>
