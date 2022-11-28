e4de436d568fc57c45abdfe73580d29ee5ecd188
Kicshikxo <supermegavalera9000@gmail.com>
2022-11-28 08:13:19 +0300

    Add rubik fontface

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'Kicshikxo\'s site',
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'preload', href: '/fonts/Rubik-Regular.woff2', as: 'font', type: 'font/woff2', crossorigin: '' },
+            ]
+        }
+    },
+
     css: [
         'primevue/resources/themes/lara-dark-blue/theme.css',
         'primevue/resources/primevue.css',
         'primeicons/primeicons.css',
-        'primeflex/primeflex.css'
+        'primeflex/primeflex.css',
+        '~/assets/css/rubik.fontface.css',
     ],
     build: {
         transpile: ['primevue']
