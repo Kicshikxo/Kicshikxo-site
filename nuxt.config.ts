3dc90fe4dce5ad6af8e78ee56159afcee657e60f
Kicshikxo <supermegavalera9000@gmail.com>
2022-11-28 07:59:07 +0300

    Add primeflex

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 export default defineNuxtConfig({
     css: [
-        'primevue/resources/themes/saga-blue/theme.css',
+        'primevue/resources/themes/lara-dark-blue/theme.css',
         'primevue/resources/primevue.css',
-        'primeicons/primeicons.css'
+        'primeicons/primeicons.css',
+        'primeflex/primeflex.css'
     ],
     build: {
         transpile: ['primevue']
