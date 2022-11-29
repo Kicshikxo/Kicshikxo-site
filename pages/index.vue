9e3a93732cd43438500521cfdb81d2aa5cbbee6e
Kicshikxo <supermegavalera9000@gmail.com>
2022-11-29 09:11:05 +0300

    Set primary color for scrolltop icon

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,13 @@
         <Repos />
         <!-- <Feature /> -->
         <Footer />
-        <PScrollTop class="bg-primary border-round w-2rem h-2rem" />
+        <PScrollTop class="bg-primary border-round w-2rem h-2rem text-orange-500" />
     </div>
 </template>
 
 <style scoped>
 :deep(.p-scrolltop) .p-scrolltop-icon {
     font-size: 1rem;
+    color: var(--primary-color-text);
 }
 </style>
