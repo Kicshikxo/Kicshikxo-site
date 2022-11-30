22ca0fc13166b7e74f1993514039e02133b165b9
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-01 02:03:09 +0300

    Remove unnecessary meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,6 @@ export default defineNuxtConfig({
             },
             title: 'Kicshikxo\'s site',
             meta: [
-                { charset: 'utf-8' },
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
                 {
                     hid: 'description',
                     name: 'description',
@@ -40,5 +38,5 @@ export default defineNuxtConfig({
 
     build: {
         transpile: ['primevue']
-    }
+    },
 })
