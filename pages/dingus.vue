9e8c0223c8219bc3eeee0dd94affbe30f9d92520
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-15 19:19:55 +0300

    Change camera far render

--- pages/dingus.vue
+++ pages/dingus.vue
@@ -28,7 +28,7 @@ onMounted(() => {
     stats = Stats()
     root.appendChild(stats.dom)
 
-    camera = new Three.PerspectiveCamera(70, root.clientWidth / root.clientHeight, 0.1, 11)
+    camera = new Three.PerspectiveCamera(70, root.clientWidth / root.clientHeight, 0.1, 100)
     camera.position.set(2, 1.5, 4)
 
     scene = new Three.Scene()
