7abee16ec0c4353ffec709d8d224ca3546483442
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-15 18:43:16 +0300

    Add three.js transpiling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,44 @@
 export default defineNuxtConfig({
-    app: {
-        head: {
-            htmlAttrs: {
-                'lang': 'ru'
-            },
-            title: 'Kicshikxo\'s site',
-            meta: [
-                {
-                    hid: 'description',
-                    name: 'description',
-                    content: 'Kicshikxo\'s portfolio website'
-                },
-            ],
-            link: [
-                { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-                { rel: 'preload', href: '/fonts/Rubik-Regular.woff2', as: 'font', type: 'font/woff2', crossorigin: '' },
-            ]
-        }
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "ru",
+      },
+      title: "Kicshikxo's site",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Kicshikxo's portfolio website",
+        },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "preload",
+          href: "/fonts/Rubik-Regular.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "",
+        },
+      ],
     },
+  },
 
-    css: [
-        'primevue/resources/themes/lara-light-blue/theme.css',
-        'primevue/resources/primevue.css',
-        'primeicons/primeicons.css',
-        'primeflex/primeflex.css',
+  css: [
+    "primevue/resources/themes/lara-light-blue/theme.css",
+    "primevue/resources/primevue.css",
+    "primeicons/primeicons.css",
+    "primeflex/primeflex.css",
 
-        '~/assets/css/rubik.fontface.css',
-        '~/assets/css/main.css',
-    ],
+    "~/assets/css/rubik.fontface.css",
+    "~/assets/css/main.css",
+  ],
 
-    modules: [
-        '@nuxtjs/color-mode',
-        '@nuxt/image-edge',
-        '@nuxtjs/robots',
-        // 'nuxt-purgecss',
-    ],
+  modules: ["@nuxtjs/color-mode", "@nuxt/image-edge", "@nuxtjs/robots"],
 
-    build: {
-        transpile: ['primevue']
-    },
-})
+  build: {
+    transpile: ["primevue", "three"],
+  },
+});
