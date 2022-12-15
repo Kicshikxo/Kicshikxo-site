9cb5144680def1a2bad4d4e51b17f06e8b9c455e
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-15 20:03:15 +0300

    Format files with prettier

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,10 +1,10 @@
-import { defineNuxtPlugin } from "#app"
-import PrimeVue from "primevue/config"
-import Button from "primevue/button"
+import { defineNuxtPlugin } from '#app'
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
 import Chip from 'primevue/chip'
-import ScrollTop from 'primevue/scrolltop';
+import ScrollTop from 'primevue/scrolltop'
 
-export default defineNuxtPlugin(nuxtApp => {
+export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: false })
     nuxtApp.vueApp.component('PButton', Button)
     nuxtApp.vueApp.component('PChip', Chip)
