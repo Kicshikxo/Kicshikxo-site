b4967c457a0ec5a72ba09193f3d59f745bbbaaf6
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-15 20:13:44 +0300

    Add progress-spinner component

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -3,10 +3,12 @@ import PrimeVue from 'primevue/config'
 import Button from 'primevue/button'
 import Chip from 'primevue/chip'
 import ScrollTop from 'primevue/scrolltop'
+import ProgressSpinner from 'primevue/progressspinner'
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: false })
     nuxtApp.vueApp.component('PButton', Button)
     nuxtApp.vueApp.component('PChip', Chip)
     nuxtApp.vueApp.component('PScrollTop', ScrollTop)
+    nuxtApp.vueApp.component('PProgressSpinner', ProgressSpinner)
 })
