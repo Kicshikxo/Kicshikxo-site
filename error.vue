4d38ff8e1541ae17c3f54725e7f421172adc35a2
Kicshikxo <supermegavalera9000@gmail.com>
2023-07-09 22:55:09 +0300

    Improve error page

--- error.vue
+++ error.vue
@@ -1,26 +1,20 @@
 <template>
-    <div>
-        <div class="h-screen flex flex-column align-items-center justify-content-center">
-            <div class="text-primary font-bold text-8xl">
-                {{ error.statusCode }}
-            </div>
-            <div class="mt-6 font-bold text-6xl text-900">
-                <span v-if="error.statusCode === '404'">Страница не найдена</span>
-                <span v-else-if="(error.statusCode === '500')">Ошибка сервера</span>
-            </div>
-            <div class="text-700 text-3xl my-6">
-                <span v-if="error.statusCode === '404'">Извините, но данная страница не существует</span>
-                <span v-else-if="(error.statusCode === '500')">Во время загрузки страницы произошла ошибка</span>
-            </div>
-            <nuxt-link v-if="error.statusCode === '404'" to="/" class="no-underline">
-                <p-button label="На главную" icon="pi pi-home" aria-label="На главную" />
-            </nuxt-link>
+    <div class="h-screen flex flex-column align-items-center justify-content-center">
+        <div class="font-bold text-8xl text-primary-600">
+            {{ error.statusCode }}
         </div>
+        <div class="font-bold text-5xl text-primary-400 mb-4">
+            <span v-if="error.statusCode === 404">Страница не найдена</span>
+            <span v-else-if="error.statusCode === 500">Ошибка сервера</span>
+        </div>
+        <nuxt-link v-if="error.statusCode === 404" to="/" class="no-underline">
+            <Button label="На главную" icon="pi pi-home" />
+        </nuxt-link>
     </div>
 </template>
 
-<script>
-export default {
-    props: ['error'],
-}
+<script setup lang="ts">
+import Button from 'primevue/button/Button.vue'
+
+defineProps<{ error: any }>()
 </script>
